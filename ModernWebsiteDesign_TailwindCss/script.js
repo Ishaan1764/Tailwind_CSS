f563f50e7b9bb3328b90629d38e4ba500315271f
Ishaan1764 <ishaansaluja1764@gmail.com>
2024-06-25 16:23:13 -0400

    made the slider (learned new js class intersectionObserver)

--- ModernWebsiteDesign_TailwindCss/script.js
+++ ModernWebsiteDesign_TailwindCss/script.js
@@ -3,3 +3,45 @@ const navDialog=document.getElementById("nav-dialog");
 function handleMenu(){
     navDialog.classList.toggle('hidden');
 }
+
+const initialTranslateLTR= -48*4;
+const initialTranslateRTL= 36*4;
+
+
+function setupIntersectionObserver(element,isLTR,speed){
+    const intersectionCallback=(entries)=>{
+        const isIntersecting=entries[0].isIntersecting;
+        if(isIntersecting){
+            document.addEventListener('scroll',scrolHandler);
+        }else{
+            document.removeEventListener('scroll',scrolHandler);
+        }
+
+        console.log(element,isIntersecting);
+        // mtlb screen me ha ke screen se bhr on scrolling.
+    }
+    const intersectionObserver=new IntersectionObserver(intersectionCallback)
+
+    intersectionObserver.observe(element);
+
+    function scrolHandler(){
+        const translateX=(window.innerHeight-element.getBoundingClientRect().top)*speed;
+        //total scroll me se top se ketna distance hai vo minus keya hai
+        //yeh pta lgane ke leye ke y mein ketna scroll hua hai aur x ,mein ab krenge. 
+        let totalTranslate=0;
+        if(isLTR){
+            totalTranslate=translateX+initialTranslateLTR;
+        }else{
+            totalTranslate=-(translateX+initialTranslateRTL);
+        }
+
+        element.style.transform=`translateX(${totalTranslate}px)`;
+    }
+}
+const line1=document.getElementById('line1');
+const line2=document.getElementById('line2');
+const line3=document.getElementById('line3');
+
+setupIntersectionObserver(line1,true,0.15);
+setupIntersectionObserver(line2,false,0.15);
+setupIntersectionObserver(line3,true,0.15);
